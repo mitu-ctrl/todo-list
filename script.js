6f3d3de2bd5874dc6d3c24d0ff964fc61be36455
mitu-ctrl <moriomakter979@gmail.com>
2024-10-24 11:05:26 +0600

    Your meaningful commit message

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("#todo");
+const btn = document.querySelector("#add-btn");
+const list = document.querySelector("#todo-list");
+
+btn.addEventListener("click", (event)=> {
+event.preventDefault()
+
+if(input.value == ""){
+        alert("Please add task");
+        return;
+    }
+
+    const listItem = document.createElement("li");
+
+    listItem.textContent = input.value;
+
+    list.appendChild(listItem);
+
+    input.value = "";
+
+
+
+})
