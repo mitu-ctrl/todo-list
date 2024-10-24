6586bda472d4daa619997a5476f2b718303b7b99
mitu-ctrl <moriomakter979@gmail.com>
2024-10-24 10:25:45 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+
+      "fontFamily": {
+'arima':['"Arima", system-ui']
+
+      }
+    },
+  },
+  plugins: [],
+}
+
